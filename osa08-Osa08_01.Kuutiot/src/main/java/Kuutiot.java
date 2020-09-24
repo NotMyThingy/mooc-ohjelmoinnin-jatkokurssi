406c12b249f60d29afabe15ebfa1a7a847f47d34
NotMyThingy <miska.janhunen@protonmail.com>
2020-09-24 18:59:34 +0300

    Ohjelma lukee merkkijonoja käyttäjältä kunnes käyttäjä syöttää merkkijonon "loppu". Mikäli syöte ei ole "loppu", ohjelma tulostaa syötetyn luvun kuutio (eli luku * luku * luku).

--- osa08-Osa08_01.Kuutiot/src/main/java/Kuutiot.java
+++ osa08-Osa08_01.Kuutiot/src/main/java/Kuutiot.java
@@ -6,5 +6,13 @@ public class Kuutiot {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
+        while (true) {
+            String input = lukija.nextLine();
+            if(input.equals("loppu")) {
+             break;
+            }
+            Integer num = Integer.valueOf(input);
+            System.out.println(num*num*num);
+        }
     }
 }
