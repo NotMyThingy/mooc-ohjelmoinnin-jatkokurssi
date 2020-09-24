27e9d100e242b8db96b600af7d5499684c609694
NotMyThingy <miska.janhunen@protonmail.com>
2020-09-24 19:21:35 +0300

    Ohjelma lukee käyttäjältä kokonaislukuja kunnes käyttäjä syöttää luvun 0. Tämän jälkeen ohjelma tulostaa syötteessä esiintyneiden positiivisten lukujen keskiarvon.
    Mikäli ohjelmassa ei syötetä yhtäkään positiivista lukua, ohjelma tulostaa "keskiarvon laskeminen ei ole mahdollista".

--- osa08-Osa08_02.PositiivistenLukujenKeskiarvo/src/main/java/PositiivistenLukujenKeskiarvo.java
+++ osa08-Osa08_02.PositiivistenLukujenKeskiarvo/src/main/java/PositiivistenLukujenKeskiarvo.java
@@ -6,5 +6,27 @@ public class PositiivistenLukujenKeskiarvo {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            int val = Integer.parseInt(lukija.nextLine());
+            if (val == 0) {
+                break;
+            }
+
+            if (val < 0) {
+                continue;
+            }
+
+            sum += val;
+            count++;
+        }
+
+        if (count == 0) {
+            System.out.println("Keskiarvon laskeminen ei ole mahdollista");
+        } else {
+            System.out.println(1.0 * sum / count);
+        }
+
     }
 }
