6db6d6e4b93238bac3fa43cc6935f0c76dcc6205
NotMyThingy <miska.janhunen@protonmail.com>
2020-09-29 11:18:04 +0300

    Periytymistä

--- osa09-Osa09_01.ABC/src/main/java/Main.java
+++ osa09-Osa09_01.ABC/src/main/java/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         // tee tänne testikoodia
-        
+        C c = new C();
+
+        c.a();
+        c.b();
+        c.c();
     }
 }
