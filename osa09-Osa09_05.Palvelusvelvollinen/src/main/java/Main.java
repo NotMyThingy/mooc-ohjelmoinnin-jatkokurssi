ff985a837f03205955b2fc1408b9a309603fb440
NotMyThingy <miska.janhunen@protonmail.com>
2020-09-30 13:00:58 +0300

    Rajapinnoista

--- osa09-Osa09_05.Palvelusvelvollinen/src/main/java/Main.java
+++ osa09-Osa09_05.Palvelusvelvollinen/src/main/java/Main.java
@@ -3,5 +3,30 @@ public class Main {
 
     public static void main(String[] args) {
         // Testaa koodiasi täällä!
+
+        Palvelusvelvollinen sivari = new Sivari();
+        System.out.println("Sivari");
+        System.out.println(sivari.paiviaJaljella());
+        sivari.palvele();
+        sivari.palvele();
+        sivari.palvele();
+        System.out.println(sivari.paiviaJaljella());
+
+        System.out.println();
+
+        System.out.println("Taistelija");
+        Palvelusvelvollinen sotilas = new Asevelvollinen(180);
+        System.out.println(sotilas.paiviaJaljella());
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        sotilas.palvele();
+        System.out.println(sotilas.paiviaJaljella());
+
     }
 }
