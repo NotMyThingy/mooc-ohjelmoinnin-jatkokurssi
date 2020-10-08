28840f7e59e4c9fbd0e79ae2fc6cd64ff077ba6f
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-08 13:26:47 +0300

    Lukujen tarkastelua virtojen avulla

--- osa10-Osa10_01.LukujenKeskiarvo/src/main/java/LukujenKeskiarvo.java
+++ osa10-Osa10_01.LukujenKeskiarvo/src/main/java/LukujenKeskiarvo.java
@@ -1,12 +1,31 @@
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LukujenKeskiarvo {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
 
+        Set<String> luetut = new HashSet<>();
+
+        System.out.println("Kirjoita syötteitä, \"loppu\" lopettaa.");
+        while (true) {
+            String luettu = lukija.nextLine();
+
+            if (luettu.equals("loppu")) {
+                break;
+            }
+
+            luetut.add(luettu);
+        }
+
+        double ka = luetut.stream()
+                .mapToInt(Integer::valueOf)
+                .average()
+                .getAsDouble();
+
+        System.out.println("Lukujen keskiarvo: " + ka);
     }
 }
