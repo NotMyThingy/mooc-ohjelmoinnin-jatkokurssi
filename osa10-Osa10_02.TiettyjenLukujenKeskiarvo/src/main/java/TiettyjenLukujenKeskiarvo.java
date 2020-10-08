0ddfe1bffda0cc3fd2e1d81caea9d24ad05a646e
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-08 15:26:15 +0300

    Ohjelma laskee positiivisten tai negatiivisten lukujen keskiarvon lukujen virrasta

--- osa10-Osa10_02.TiettyjenLukujenKeskiarvo/src/main/java/TiettyjenLukujenKeskiarvo.java
+++ osa10-Osa10_02.TiettyjenLukujenKeskiarvo/src/main/java/TiettyjenLukujenKeskiarvo.java
@@ -1,12 +1,47 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TiettyjenLukujenKeskiarvo {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<Integer> luvut = new ArrayList<>();
 
+        System.out.println("Kirjoita syötteitä, \"loppu\" lopettaa.");
+        while (true) {
+            String luettu = lukija.nextLine();
+
+            if (luettu.equals("loppu")) {
+                break;
+            }
+
+            try {
+                int luku = Integer.parseInt(luettu);
+                luvut.add(luku);
+            } catch (NumberFormatException ex) {
+                System.out.println("Epäkelpo syöte.");
+            }
+        }
+
+        System.out.println("Tulostetaanko negatiivisten vai positiivisten lukujen keskiarvo? (n/p)");
+        String luettu = lukija.nextLine();
+
+        if (luettu.equals("n")) {
+            System.out.println("Negatiivisten lukujen keskiarvo:" + luvut.stream()
+                    .mapToInt(i -> i)
+                    .filter(i -> i < 0)
+                    .average()
+                    .getAsDouble());
+        }
+
+        if (luettu.equals("p")) {
+            System.out.println("Positiivisten lukujen keskiarvo: " + luvut.stream()
+                    .mapToInt(i -> i)
+                    .filter(i -> i > 0)
+                    .average()
+                    .getAsDouble());
+        }
     }
 }
