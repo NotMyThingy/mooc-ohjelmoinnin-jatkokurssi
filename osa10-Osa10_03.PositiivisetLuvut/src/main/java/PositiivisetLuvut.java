8b63d0e9f89a69e43614c2e665fa7310f2207985
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-08 16:55:30 +0300

    Virtaa ja Collectors

--- osa10-Osa10_03.PositiivisetLuvut/src/main/java/PositiivisetLuvut.java
+++ osa10-Osa10_03.PositiivisetLuvut/src/main/java/PositiivisetLuvut.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -7,8 +8,21 @@ public class PositiivisetLuvut {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        // testaa metodia täällä
 
+        List<Integer> lista = new ArrayList<>();
+        lista.add(3);
+        lista.add(7);
+        lista.add(-4);
+        lista.add(2);
+        lista.add(6);
+
+        System.out.println(positiiviset(lista));
+    }
+
+    public static List<Integer> positiiviset(List<Integer> luvut) {
+        return luvut.stream()
+                .filter(i -> i > 0)
+                .collect(Collectors.toList());
     }
 
 }
