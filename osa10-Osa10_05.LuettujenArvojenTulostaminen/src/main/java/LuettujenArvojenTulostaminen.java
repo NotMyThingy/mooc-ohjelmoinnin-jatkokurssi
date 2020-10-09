7b11bce09fde1eb1fd97570a93c188e36dc66eaa
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-09 21:16:32 +0300

    Luettujen arvojen tulostamista virrasta

--- osa10-Osa10_05.LuettujenArvojenTulostaminen/src/main/java/LuettujenArvojenTulostaminen.java
+++ osa10-Osa10_05.LuettujenArvojenTulostaminen/src/main/java/LuettujenArvojenTulostaminen.java
@@ -1,11 +1,25 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LuettujenArvojenTulostaminen {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        List<String> luetut = new ArrayList<>();
 
+        while (true) {
+            String luettu = lukija.nextLine();
+
+            if (luettu.isEmpty()) {
+                break;
+            }
+
+            luetut.add(luettu);
+        }
+
+        luetut.stream()
+                .forEach(luku -> System.out.println(luku));
     }
 }
