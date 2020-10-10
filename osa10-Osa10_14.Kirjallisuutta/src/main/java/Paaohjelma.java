f0ea7e3c870df95fe6c045cd790a169357e68b3a
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-10 16:31:00 +0300

    Ohjelma kysyy kirjoja tulostaa syötettyjen kirjojen lukumäärän sekä kirjat kohdeiän ja aakkosten mukaan järjestyksessä.

--- osa10-Osa10_14.Kirjallisuutta/src/main/java/Paaohjelma.java
+++ osa10-Osa10_14.Kirjallisuutta/src/main/java/Paaohjelma.java
@@ -1,11 +1,44 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Paaohjelma {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        List<Kirja> kirjat = new ArrayList<>();
 
+        while (true) {
+            System.out.print("Syötä kirjan nimi, tyhjä lopettaa: ");
+            String nimi = lukija.nextLine();
+            if (nimi.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Syötä kirjan pienin kohdeikä: ");
+            int ika = Integer.parseInt(lukija.nextLine());
+
+            Kirja k = new Kirja(nimi, ika);
+            if (kirjat.contains(k)) {
+                continue;
+            }
+
+            kirjat.add(k);
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println(kirjat.size() + " kirjaa");
+        System.out.println();
+
+        Comparator<Kirja> vertailija = Comparator
+                .comparing(Kirja::getMinkohdeika)
+                .thenComparing(Kirja::getNimi);
+
+        Collections.sort(kirjat, vertailija);
+
+        System.out.println("Kirjat");
+        kirjat.stream()
+                .forEach(System.out::println);
     }
 
 }
