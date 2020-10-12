d7820f30bb87114ee87f6e9466958551ff32caed
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-12 20:30:28 +0300

    Perintää

---
+++ osa11-Osa11_03.NaytosJaLippu/src/main/java/Naytos.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Naytos {
+
+    private String elokuva;
+    private String aika;
+}
---
+++ osa11-Osa11_04.OpiskelijaJaKorkeakoulu/src/main/java/Korkeakoulu.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Korkeakoulu {
+
+    private ArrayList<Opiskelija>opiskelijat;
+    private String nimi;
+
+}
