cbd1497e1366f7927900c38c07509fd2f03f2201
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-14 15:28:20 +0300

    Taulokoiden rajaamista

--- osa12-Osa12_03.RajatutTaulukossa/src/main/java/Ohjelma.java
+++ osa12-Osa12_03.RajatutTaulukossa/src/main/java/Ohjelma.java
@@ -1,12 +1,34 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ohjelma {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        // voit testata luokkaasi täällä
 
+        int[] luvut = {8, -2, 3, 1, 1};
+
+        System.out.println(summa(luvut, 0, luvut.length, 0, 999));
+
+    }
+
+    public static int summa(int[] taulukko, int mista, int mihin, int pienin, int suurin) {
+        if (mista < 0) {
+            mista = 0;
+        }
+        if (mihin > taulukko.length) {
+            mihin = taulukko.length;
+        }
+
+        int summa = 0;
+        for (int i = mista; i < mihin; i++) {
+            if (taulukko[i] >= pienin && taulukko[i] <= suurin) {
+                summa += taulukko[i];
+            }
+        }
+
+        return summa;
     }
 
 }
