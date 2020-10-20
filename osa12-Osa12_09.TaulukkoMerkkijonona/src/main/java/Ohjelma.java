91a6e1b23bdf22dd27d303dd602425faeda91371
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-20 10:54:53 +0300

    Arvojen tulostaminen kaksiulotteisesta taulukosta

--- osa12-Osa12_09.TaulukkoMerkkijonona/src/main/java/Ohjelma.java
+++ osa12-Osa12_09.TaulukkoMerkkijonona/src/main/java/Ohjelma.java
@@ -3,6 +3,27 @@ public class Ohjelma {
 
     public static void main(String[] args) {
         // Testaa metodiasi täällä
+        int[][] matriisi = {
+                {3, 2, 7, 6},
+                {2, 4, 1, 0},
+                {3, 2, 1, 0}
+        };
+
+        System.out.println(taulukkoMerkkijonona(matriisi));
+    }
+
+
+    public static String taulukkoMerkkijonona(int[][] taulukko) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int[] rivi : taulukko) {
+            for (int luku : rivi) {
+                stringBuilder.append(luku);
+            }
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
     }
 
 }
