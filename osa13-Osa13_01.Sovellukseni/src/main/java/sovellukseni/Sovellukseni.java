6865c833941bfa023579becd42b4128de92f6692
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-20 14:31:32 +0300

    Graafiset käyttöliittymät - JavaFX

--- osa13-Osa13_01.Sovellukseni/src/main/java/sovellukseni/Sovellukseni.java
+++ osa13-Osa13_01.Sovellukseni/src/main/java/sovellukseni/Sovellukseni.java
@@ -1,11 +1,19 @@
 package sovellukseni;
 
 
-public class Sovellukseni {
-    
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Sovellukseni extends Application {
+
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(Sovellukseni.class);
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sovellukseni");
+        primaryStage.show();
+    }
 }
