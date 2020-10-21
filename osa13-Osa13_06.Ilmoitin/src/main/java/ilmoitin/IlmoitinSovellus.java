910f8ba3bfd68f8c7b6b51d4cccc8cdb91267126
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-21 14:20:57 +0300

    Tapahtumankäsittelijä lisätty

--- osa13-Osa13_06.Ilmoitin/src/main/java/ilmoitin/IlmoitinSovellus.java
+++ osa13-Osa13_06.Ilmoitin/src/main/java/ilmoitin/IlmoitinSovellus.java
@@ -1,11 +1,36 @@
 package ilmoitin;
 
 
-public class IlmoitinSovellus {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class IlmoitinSovellus extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(IlmoitinSovellus.class);
     }
 
+    @Override
+    public void start(Stage stage) {
+        TextField textField = new TextField();
+        Button updateButton = new Button("Update");
+        Label resultLabel = new Label();
+
+        updateButton.setOnAction((event) -> resultLabel.setText(textField.getText()));
+
+        VBox vBox = new VBox();
+        vBox.setSpacing(15);
+        vBox.getChildren().addAll(textField, updateButton, resultLabel);
+
+        Scene scene = new Scene(vBox);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 }
