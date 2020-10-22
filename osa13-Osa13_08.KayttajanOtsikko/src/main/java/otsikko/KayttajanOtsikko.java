3afae59412fa866a5962bf88a884595a8553895b
NotMyThingy <miska.janhunen@protonmail.com>
2020-10-22 11:34:13 +0300

    Sovelluksen käynnistäminen luokan ulkopuolelta

--- osa13-Osa13_08.KayttajanOtsikko/src/main/java/otsikko/KayttajanOtsikko.java
+++ osa13-Osa13_08.KayttajanOtsikko/src/main/java/otsikko/KayttajanOtsikko.java
@@ -1,7 +1,21 @@
 package otsikko;
 
 
-public class KayttajanOtsikko {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class KayttajanOtsikko extends Application {
 
 
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parameters params = getParameters();
+            String otsikko = params.getNamed().get("otsikko");
+            stage.setTitle(otsikko);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        stage.show();
+    }
 }
--- osa13-Osa13_08.KayttajanOtsikko/src/main/java/otsikko/Main.java
+++ osa13-Osa13_08.KayttajanOtsikko/src/main/java/otsikko/Main.java
@@ -1,11 +1,17 @@
 package otsikko;
 
 
+import javafx.application.Application;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Otsikko: ");
+        String otsikko = new Scanner(System.in).nextLine();
 
+        Application.launch(KayttajanOtsikko.class, "--otsikko=" + otsikko);
     }
 
 }
