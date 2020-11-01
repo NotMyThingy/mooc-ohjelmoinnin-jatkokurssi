48190fb78726f16d42f7ea4ac108aa950a0e800c
NotMyThingy <miska.janhunen@protonmail.com>
2020-11-01 19:37:16 +0200

    risti-nolla peli graafisella UI:lla. #javafx

---
+++ osa13-Osa13_13.Ristinolla/src/main/java/ristinolla/Ristinolla.java
@@ -0,0 +1,97 @@
+package ristinolla;
+
+import java.util.Arrays;
+
+public class Ristinolla {
+
+	private String vuoro;
+	private String[][] ristinolla;
+
+	public Ristinolla() {
+		this.vuoro = "X";
+		this.ristinolla = new String[][]{
+				{" ", " ", " "},
+				{" ", " ", " "},
+				{" ", " ", " "}
+		};
+	}
+
+	public String vuoro() {
+		return this.vuoro;
+	}
+
+	public String status(int x, int y) {
+		if (x < 0 || x > 2 || y < 0 || y > 2) {
+			return "";
+		}
+
+		return this.ristinolla[x][y];
+	}
+
+	public void setMark(int x, int y) {
+		if (x < 0 || x > 2 || y < 0 || y > 2) {
+			return;
+		}
+
+		if (!this.ristinolla[x][y].equals(" ")) {
+			return;
+		}
+
+		if (loppu()) {
+			return;
+		}
+
+		this.ristinolla[x][y] = this.vuoro;
+
+		if (this.vuoro.equals("X")) {
+			this.vuoro = "O";
+		} else {
+			this.vuoro = "X";
+		}
+
+	}
+
+	public boolean loppu() {
+		int[][] samat = {
+				{0, 1, 2},
+				{3, 4, 5},
+				{6, 7, 8},
+				{0, 3, 6},
+				{1, 4, 7},
+				{2, 5, 8},
+				{0, 4, 8},
+				{2, 5, 6}
+		};
+
+		for (int i = 0; i < samat.length; i++) {
+			if (tablesMatch(samat[i])) {
+				return true;
+			}
+		}
+
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				if (status(x, y).trim().isEmpty()) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private boolean tablesMatch(int[] taulukko) {
+		String[] arvot = {getArvo(taulukko[0]), getArvo(taulukko[1]), getArvo(taulukko[2])};
+		Arrays.sort(arvot);
+		return arvot[0].equals(arvot[1])
+				&& arvot[0].equals(arvot[2])
+				&& !arvot[0].trim().isEmpty();
+	}
+
+	private String getArvo(int pos) {
+		int x = pos % 3;
+		int y = pos / 3;
+
+		return this.ristinolla[x][y];
+	}
+}
--- osa13-Osa13_13.Ristinolla/src/main/java/ristinolla/RistinollaSovellus.java
+++ osa13-Osa13_13.Ristinolla/src/main/java/ristinolla/RistinollaSovellus.java
@@ -2,12 +2,82 @@ package ristinolla;
 
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class RistinollaSovellus {
 
+public class RistinollaSovellus extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		Ristinolla ristinolla = new Ristinolla();
+
+		Font font = Font.font("Monospaced", 40);
+
+		Label pelitilanne = new Label("Vuoro: " + ristinolla.vuoro());
+		pelitilanne.setPadding(new Insets(0, 30, 0, 30));
+		pelitilanne.setFont(font);
+
+		BorderPane layout = new BorderPane();
+		layout.setPrefSize(300, 300);
+		layout.setTop(pelitilanne);
+
+		GridPane peliruudukko = new GridPane();
+		peliruudukko.setHgap(10);
+		peliruudukko.setVgap(10);
+		peliruudukko.setPadding(new Insets(10, 30, 30, 30));
+
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				Button button = new Button(ristinolla.status(x, y));
+				button.setFont(font);
+
+				peliruudukko.add(button, x, y);
+
+				int rx = x;
+				int ry = y;
+
+				button.setOnMouseClicked((event) -> {
+					ristinolla.setMark(rx, ry);
+					button.setText(ristinolla.status(rx, ry));
+					pelitilanne.setText("Vuoro: " + ristinolla.vuoro());
+
+					if (ristinolla.loppu()) {
+						pelitilanne.setText("Loppu!");
+
+						for (Node node : peliruudukko.getChildren()) {
+							Button btn = (Button) node;
+							btn.setDisable(true);
+						}
+					}
+				});
+			}
+		}
+
+		layout.setCenter(peliruudukko);
+		Scene scene = new Scene(layout);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+
+	/*private void lukitseRuudukko() {
+		for (Node node : peliruudukko.getChildren()) {
+			Button button = (Button) node;
+			button.setDisable(true);
+		}
+	}*/
 
 	public static void main(String[] args) {
-		Application.launch(UI.class);
+		Application.launch();
 	}
 
 }
