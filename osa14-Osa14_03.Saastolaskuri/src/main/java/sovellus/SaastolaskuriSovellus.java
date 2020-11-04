c1f108da5bb1953cf456c3d20aa8fdb8aaa7a5f5
NotMyThingy <miska.janhunen@protonmail.com>
2020-11-04 22:26:41 +0200

    Toimiva versio, tmc antaa virheen

--- osa14-Osa14_03.Saastolaskuri/src/main/java/sovellus/SaastolaskuriSovellus.java
+++ osa14-Osa14_03.Saastolaskuri/src/main/java/sovellus/SaastolaskuriSovellus.java
@@ -14,106 +14,17 @@ import javafx.stage.Stage;
 
 public class SaastolaskuriSovellus extends Application {
 
+	private LineChart lineChart;
+	private int monthlyInvestment;
+
 	@Override
 	public void start(Stage stage) {
 		BorderPane layout = new BorderPane();
 
-		/**
-		 * Liukurit
-		 */
-		VBox controls = new VBox();
-
-		BorderPane investmentsPane = new BorderPane();
-		BorderPane interestsPane = new BorderPane();
-
-		/**
-		 * YLEMPI LIUKURI
-		 */
-		final Slider amoutSlider = new Slider(25, 250, 25);
-		amoutSlider.setShowTickMarks(true);
-		amoutSlider.setShowTickLabels(true);
-		amoutSlider.setPadding(new Insets(10));
-
-		Label depositLeftLabel = new Label("Kuukausittainen tallennus");
-		Label depositRightLabel = new Label(String.valueOf(amoutSlider.getValue()));
-
-		depositLeftLabel.setPadding(new Insets(10));
-		depositRightLabel.setPadding(new Insets(10));
-
-		/**
-		 * ALEMPI LIUKURI
-		 */
-		final Slider interestSlider = new Slider(0, 10, 0);
-		interestSlider.setShowTickMarks(true);
-		interestSlider.setShowTickLabels(true);
-		interestSlider.setPadding(new Insets(10));
-
-		Label interestLeftLabel = new Label("Vuosittainen korko");
-		Label interestRightLabel = new Label("0.0");
-
-		interestLeftLabel.setPadding(new Insets(10));
-		interestRightLabel.setPadding(new Insets(10));
-
-		/**
-		 * KOMPONENTIT BOXIIN
-		 */
-		investmentsPane.setLeft(depositLeftLabel);
-		investmentsPane.setCenter(amoutSlider);
-		investmentsPane.setRight(depositRightLabel);
-
-		interestsPane.setLeft(interestLeftLabel);
-		interestsPane.setCenter(interestSlider);
-		interestsPane.setRight(interestRightLabel);
-
-		controls.setPadding(new Insets(15));
-		controls.getChildren().addAll(investmentsPane, interestsPane);
-
-		/**
-		 * VIIVAKAAVIO
-		 */
-		NumberAxis xAxis = new NumberAxis(0, 30, 1);
-		NumberAxis yAxis = new NumberAxis();
-
-		LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-		lineChart.setAnimated(false);
-		lineChart.setTitle("Säästölaskuri");
-
-		/**
-		 * LIUKUREIDEN TOIMINNALLISUUS
-		 */
-		XYChart.Series depositSeries = new XYChart.Series();
-		amoutSlider.valueProperty().addListener((observableValue, oldValue, newValue) -> {
-			depositRightLabel.setText(String.valueOf(newValue.intValue()));
-
-			lineChart.getData().remove(depositSeries);
-			depositSeries.getData().clear();
-			for (int year = 0; year <= 30; year++) {
-				depositSeries.getData().add(year, new XYChart.Data(year, (newValue.intValue() * 12) * year));
-			}
-			lineChart.getData().add(depositSeries);
-		});
-
-		XYChart.Series interestSeries = new XYChart.Series();
-		interestSlider.valueProperty().addListener((observableValue, oldValue, newValue) -> {
-			interestRightLabel.setText(String.format("%.2f", newValue.doubleValue()));
-
-			lineChart.getData().remove(interestSeries);
-			interestSeries.getData().clear();
-
-			double paaoma = 0;
-			int talletus = (int) amoutSlider.getValue();
-			for (int year = 0; year <= 30; year++) {
-				interestSeries.getData().add(new XYChart.Data(year, paaoma));
-				System.out.println("Vuosi: " + year + " - Kertyma: " + paaoma);
-				paaoma = (paaoma + (talletus * 12)) * (newValue.doubleValue() / 100 + 1);
-			}
-			lineChart.getData().add(interestSeries);
-		});
-		/**
-		 * KOMPONENTIT ALUSTAAN
-		 */
-
+		VBox controls = createControls();
 		layout.setTop(controls);
+
+		lineChart = creteNewChart();
 		layout.setCenter(lineChart);
 
 		Scene scene = new Scene(layout, 640, 480);
@@ -123,27 +34,104 @@ public class SaastolaskuriSovellus extends Application {
 	}
 
 
-	/*
+	private VBox createControls() {
+		final VBox controls = new VBox();
+		controls.setPadding(new Insets(15));
+
+		final BorderPane topPane = createInvestmentSlider();
+		final BorderPane bottomPane = createInterestSlider();
+
+		controls.getChildren().addAll(topPane, bottomPane);
+
+		return controls;
+	}
 
 
-	 */
+	private BorderPane createInvestmentSlider() {
+		final BorderPane investmentPane = new BorderPane();
 
-	/**
-	 * ALEMPI LIUKUVALITSIN KOROLLE
-	 *//*
-	private BorderPane createInterestSlider() {
-		BorderPane bottomPane = new BorderPane();
+		final Slider investment = new Slider(25, 250, 25);
+		investment.setShowTickMarks(true);
+		investment.setShowTickLabels(true);
+		investment.setPadding(new Insets(10));
 
+		final Label depositLeftLabel = new Label("Kuukausittainen tallennus");
+		final Label depositRightLabel = new Label(String.valueOf(investment.getValue()));
 
-		interestSlider.valueProperty().addListener((observableValue, oldValue, newValue) -> {
-			interestRight.setText(String.format("%.2f", interestSlider.getValue()));
+		depositLeftLabel.setPadding(new Insets(10));
+		depositRightLabel.setPadding(new Insets(10));
 
+		investmentPane.setLeft(depositLeftLabel);
+		investmentPane.setCenter(investment);
+		investmentPane.setRight(depositRightLabel);
 
+		final XYChart.Series depositSeries = new XYChart.Series();
+		investment.valueProperty().addListener((observableValue, oldValue, newValue) -> {
+			monthlyInvestment = newValue.intValue();
+			depositRightLabel.setText(String.valueOf(newValue.intValue()));
+
+			lineChart.getData().remove(depositSeries);
+			depositSeries.getData().clear();
+			for (int year = 0; year <= 30; year++) {
+				depositSeries.getData().add(year, new XYChart.Data(year, (monthlyInvestment * 12) * year));
+			}
+			lineChart.getData().add(depositSeries);
 		});
 
-		return bottomPane;
+		return investmentPane;
 	}
-*/
+
+
+	private BorderPane createInterestSlider() {
+		final BorderPane interestPane = new BorderPane();
+
+		final Slider interest = new Slider(0, 10, 0);
+		interest.setShowTickMarks(true);
+		interest.setShowTickLabels(true);
+		interest.setPadding(new Insets(10));
+
+		final Label interestLeftLabel = new Label("Vuosittainen korko");
+		final Label interestRightLabel = new Label("0.0");
+
+		interestLeftLabel.setPadding(new Insets(10));
+		interestRightLabel.setPadding(new Insets(10));
+
+		interestPane.setLeft(interestLeftLabel);
+		interestPane.setCenter(interest);
+		interestPane.setRight(interestRightLabel);
+
+		final XYChart.Series interestSeries = new XYChart.Series();
+		interest.valueProperty().addListener((observableValue, oldValue, newValue) -> {
+			interestRightLabel.setText(String.format("%.2f", newValue.doubleValue()));
+
+			lineChart.getData().remove(interestSeries);
+			interestSeries.getData().clear();
+
+			double savings = 0.0;
+			for (int year = 0; year <= 30; year++) {
+				interestSeries.getData().add(new XYChart.Data(year, savings));
+				savings = (savings + (monthlyInvestment * 12)) * (newValue.doubleValue() / 100 + 1);
+			}
+			interestSeries.getData().stream().forEach(i -> System.out.println(i));
+
+			lineChart.getData().add(interestSeries);
+		});
+
+		return interestPane;
+	}
+
+	private LineChart<Number, Number> creteNewChart() {
+		NumberAxis xAxis = new NumberAxis(0, 30, 1);
+		NumberAxis yAxis = new NumberAxis();
+
+		LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+		lineChart.setAnimated(false);
+		lineChart.setTitle("Säästölaskuri");
+
+		return lineChart;
+	}
+
+
 	public static void main(String[] args) {
 		Application.launch();
 	}
