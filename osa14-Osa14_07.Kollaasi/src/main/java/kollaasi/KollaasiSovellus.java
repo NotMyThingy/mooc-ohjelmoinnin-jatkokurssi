bf3be00bcb403666b921d1861c9499af6e57cd96
NotMyThingy <miska.janhunen@protonmail.com>
2020-11-11 21:11:09 +0200

    Kuvan kasitteya - warhol

--- osa14-Osa14_07.Kollaasi/src/main/java/kollaasi/KollaasiSovellus.java
+++ osa14-Osa14_07.Kollaasi/src/main/java/kollaasi/KollaasiSovellus.java
@@ -1,7 +1,9 @@
 package kollaasi;
 
 import javafx.application.Application;
+
 import static javafx.application.Application.launch;
+
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -14,53 +16,58 @@ import javafx.stage.Stage;
 
 public class KollaasiSovellus extends Application {
 
-    @Override
-    public void start(Stage stage) {
+	@Override
+	public void start(Stage stage) {
 
-        // esimerkki avaa kuvan, luo uuden kuvan, ja kopioi avatun kuvan 
-        // uuteen kuvaan pikseli kerrallaan
-        Image lahdeKuva = new Image("file:monalisa.png");
+		// esimerkki avaa kuvan, luo uuden kuvan, ja kopioi avatun kuvan
+		// uuteen kuvaan pikseli kerrallaan
+		Image lahdeKuva = new Image("file:monalisa.png");
 
-        PixelReader kuvanLukija = lahdeKuva.getPixelReader();
+		PixelReader kuvanLukija = lahdeKuva.getPixelReader();
 
-        int leveys = (int) lahdeKuva.getWidth();
-        int korkeus = (int) lahdeKuva.getHeight();
+		int leveys = (int) lahdeKuva.getWidth();
+		int korkeus = (int) lahdeKuva.getHeight();
 
-        WritableImage kohdeKuva = new WritableImage(leveys, korkeus);
-        PixelWriter kuvanKirjoittaja = kohdeKuva.getPixelWriter();
+		WritableImage kohdeKuva = new WritableImage(leveys, korkeus);
+		PixelWriter kuvanKirjoittaja = kohdeKuva.getPixelWriter();
 
-        int yKoordinaatti = 0;
-        while (yKoordinaatti < korkeus) {
-            int xKoordinaatti = 0;
-            while (xKoordinaatti < leveys) {
+		// KIRJOITETAAN KAIKKI NELJA KULMAA
+		for (int x = 0; x < leveys / 2; x++) {
+			for (int y = 0; y < korkeus / 2; y++) {
+				Color color = kuvanLukija.getColor(x * 2, y * 2);
 
-                Color vari = kuvanLukija.getColor(xKoordinaatti, yKoordinaatti);
-                double punainen = vari.getRed();
-                double vihrea = vari.getGreen();
-                double sininen = vari.getBlue();
-                double lapinakyvyys = vari.getOpacity();
+				kuvanKirjoittaja.setColor(x, y, color);
+				kuvanKirjoittaja.setColor(x + leveys / 2, y, color);
+				kuvanKirjoittaja.setColor(x, y + korkeus / 2, color);
+				kuvanKirjoittaja.setColor(x + leveys / 2, y + korkeus / 2, color);
+			}
+		}
 
-                Color uusiVari = new Color(punainen, vihrea, sininen, lapinakyvyys);
+		// WARHOL
+		kuvanLukija = kohdeKuva.getPixelReader();
+		for (int x = 0; x < leveys; x++) {
+			for (int y = 0; y < korkeus; y++) {
+				Color color = kuvanLukija.getColor(x, y);
 
-                kuvanKirjoittaja.setColor(xKoordinaatti, yKoordinaatti, uusiVari);
+				double red = 1 - color.getRed();
+				double green = 1 - color.getGreen();
+				double blue = 1 - color.getBlue();
 
-                xKoordinaatti++;
-            }
+				kuvanKirjoittaja.setColor(x, y, new Color(red, green, blue, color.getOpacity()));
+			}
+		}
 
-            yKoordinaatti++;
-        }
+		ImageView kuva = new ImageView(kohdeKuva);
 
-        ImageView kuva = new ImageView(kohdeKuva);
+		Pane pane = new Pane();
+		pane.getChildren().add(kuva);
 
-        Pane pane = new Pane();
-        pane.getChildren().add(kuva);
+		stage.setScene(new Scene(pane));
+		stage.show();
+	}
 
-        stage.setScene(new Scene(pane));
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
